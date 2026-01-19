1d4edc04581a440f76e8842bb33d0a60d7de069e
sohailiqrar <sohailiqrar22@gmail.com>
2026-01-19 23:13:25 +0530

    "return the variable"

--- producer/eventhub_client.py
+++ producer/eventhub_client.py
@@ -5,7 +5,9 @@ def get_eventhub_producer():
     conn_str = os.getenv("EVENTHUB_CONNECTION_STRING")
     hub_name = os.getenv("EVENTHUB_NAME")
 
-    return EventHubProducerClient.from_connection_string(
+    Event_hub = EventHubProducerClient.from_connection_string(
         conn_str=conn_str,
         eventhub_name=hub_name
     )
+
+    return Event_hub
